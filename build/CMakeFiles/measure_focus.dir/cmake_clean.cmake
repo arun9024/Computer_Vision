eac154142f18fdc966009aa7c6e8ec9f334e5f07
Arun Verma <arun9024@gmail.com>
2018-06-18 03:05:03 +0200

    some files are added

---
+++ build/CMakeFiles/measure_focus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/measure_focus.dir/src/main.cpp.o"
+  "measure_focus.pdb"
+  "measure_focus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/measure_focus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
